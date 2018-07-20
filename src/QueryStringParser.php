e1d398e17bb82a12cf46d92aff259793518a66ed
Jen Wachter <jenleighkelly@gmail.com>
2018-07-20 14:25:50 -0400

    Added year and type parsing

--- src/QueryStringParser.php
+++ src/QueryStringParser.php
@@ -14,9 +14,9 @@ class QueryStringParser
     // deafult page number
     $page = 1;
 
-    if (isset($_GET['page']) && $pageAsInt = (int) $_GET['page']) {
+    if (isset($_GET['page']) && $int = (int) $_GET['page']) {
       // valid number set in query string
-      return $pageAsInt;
+      return $int;
     }
 
     return $page;
@@ -32,7 +32,7 @@ class QueryStringParser
     if (isset($_GET['q']) && !empty($_GET['q'])) {
       return urlencode($_GET['q']);
     } else {
-      throw new Exception('Please provide a query via the "q" query string parameter');
+      return null;
     }
   }
 
@@ -47,6 +47,42 @@ class QueryStringParser
     } else {
       return null;
     }
+  }
 
+  public function getYear()
+  {
+    if (!isset($_GET['year'])) {
+      return null;
+
+    }
+
+    $int = (int) $_GET['year'];
+
+    if ($int < 1994) {
+      return null;
+    }
+
+    return [
+      strtotime("{$int}-01-01 00:00:00 GMT"),
+      strtotime("{$int}-12-31 23:59:59 GMT")
+    ];
+  }
+
+  public function getType()
+  {
+    if (!isset($_GET['type'])) {
+      return null;
+    }
+
+    $types = ['article', 'announcement', 'event', 'page'];
+    $valid = [];
+
+    foreach (explode(',', $_GET['type']) as $type) {
+      if (in_array($type, $types)) {
+        $valid[] = $type;
+      }
+    }
+
+    return $valid;
   }
 }
